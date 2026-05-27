4adc3eaed4e576c96ab4377ca94a28447da1235d
julieberg104 <julieberg104@gmail.com>
2026-05-26 22:56:42 -0400

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Simple Interest Calculator
+
+echo "Enter principal:"
+read p
+
+echo "Enter rate:"
+read r
+
+echo "Enter time:"
+read t
+
+si=$((p*r*t/100))
+
+echo "Simple Interest is:"
+echo $si
